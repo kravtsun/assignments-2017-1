cf9cd57eaff6a4e169e15a5e281f541f9452873b
Andrey Kravtsun <kravtsun@cvisionlab.com>
2017-03-15 16:29:24 +0300

    Infect StringSetImpl code with testing serialization procedures between tests (where it does not affect performance much).

---
+++ src/test/java/ru/spbau/mit/StreamSerializableTest.java
@@ -0,0 +1,74 @@
+package ru.spbau.mit;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+public class StreamSerializableTest {
+    @Test(expected = SerializationException.class)
+    public void testFailEmpty() {
+        testDeserialization(new ByteArrayOutputStream());
+    }
+
+    @Test(expected = SerializationException.class)
+    public void testFailDummyHeader() {
+        final int dummyHeader = 0xBABEFAFA;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        writeInteger(dummyHeader, out);
+        testDeserialization(out);
+    }
+
+    @Test
+    public void testEmpty() {
+        StringSetImpl s = new StringSetImpl();
+        assertEmptyStringSetImpl(s); // just for sureness.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        s.serialize(out);
+        StringSetImpl s1 = testDeserialization(out);
+        assertEmptyStringSetImpl(s1);
+
+        String[] arr = {"abc", "cde", ""};
+        for (String str : arr) {
+            assertTrue(s1.add(str));
+        }
+
+        for (String str : arr) {
+            assertTrue(s1.remove(str));
+        }
+
+        out.reset();
+        s1.serialize(out);
+
+        StringSetImpl s2 = testDeserialization(out);
+        assertEmptyStringSetImpl(s2);
+    }
+
+    private static void writeInteger(int num, OutputStream out) {
+        final int bytesInInteger = 4;
+        final int bitsInByte = 8;
+        try {
+            for (int i = 0; i < bytesInInteger; ++i) {
+                final int lowestByte = num & ((1 << bitsInByte) - 1);
+                num >>= bitsInByte;
+                out.write(lowestByte);
+            }
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    private static StringSetImpl testDeserialization(ByteArrayOutputStream out) throws SerializationException {
+        StringSetImpl stringSet = new StringSetImpl();
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        ((StreamSerializable)stringSet).deserialize(in);
+        return stringSet;
+    }
+
+    private static void assertEmptyStringSetImpl(StringSetImpl s) {
+        assertNotNull(s);
+        assertEquals(0, s.size());
+        assertEquals(0, s.howManyStartsWithPrefix(""));
+    }
+}
--- src/test/java/ru/spbau/mit/StringSetImplTest.java
+++ src/test/java/ru/spbau/mit/StringSetImplTest.java
@@ -1,7 +1,11 @@
 package ru.spbau.mit;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -10,9 +14,10 @@ import static org.junit.Assert.*;
 public class StringSetImplTest {
     private static final int MAX_STRING_SIZE = 100;
     private static final int TESTS_COUNT = 10000;
-
     private static final Random RANDOMIZER;
     private static final String RANDOM_SYMBOLS;
+    private ArrayList<StringSetImpl> stringSets;
+    private boolean testSerializationBetweenStates;
 
     static {
         final int randomizerSeed = 1123834521;
@@ -30,6 +35,117 @@ public class StringSetImplTest {
         RANDOM_SYMBOLS = sb.toString();
     }
 
+    @Before
+    public void setUp() throws Exception {
+        stringSets = new ArrayList<>();
+        stringSets.add(new StringSetImpl());
+        testSerializationBetweenStates = true;
+    }
+
+    @Test
+    public void testSimple() {
+        StringSetImpl stringSet = new StringSetImpl();
+        assertAdd(true, ("abc"));
+        assertContains(true, ("abc"));
+        assertSize(1);
+        assertHowManyStartsWithPrefix("abc", 1);
+    }
+
+    @Test
+    public void myTest() {
+        StringSetImpl stringSet = new StringSetImpl();
+        ArrayList<String> alreadyAdded = new ArrayList<>();
+        testSerializationBetweenStates = false;
+        for (int i = 0; i < TESTS_COUNT; i++) {
+            String newString = randomString();
+            assertSize(alreadyAdded.size());
+            if (alreadyAdded.contains(newString)) {
+                assertAdd(false, (newString));
+                assertSize(alreadyAdded.size());
+            } else {
+                assertAdd(true, (newString));
+                alreadyAdded.add(newString);
+            }
+        }
+
+        for (String s : alreadyAdded) {
+            assertAdd(false, (s));
+            assertContains(true, (s));
+        }
+
+        for (int i = 0; i < TESTS_COUNT; i++) {
+            String s = randomString();
+            assertContains(alreadyAdded.contains(s), s);
+        }
+        testSerializationBetweenStates = true;
+    }
+
+    @Test
+    public void stringSetSpecificCases() {
+        StringSetImpl stringSet = new StringSetImpl();
+        String newString = randomString();
+        assertSize(0);
+        assertAdd(true, (newString));
+        assertSize(1);
+        assertAdd(false, (newString));
+        assertSize(1);
+        String prefix = newString.substring(0, newString.length() - 1);
+        assertAdd(true, (prefix));
+        assertRemove(true, (newString));
+        assertContains(true, (prefix));
+        assertContains(false, (newString));
+    }
+
+    @Test
+    public void typicalCase1() {
+        StringSetImpl stringSet = new StringSetImpl();
+        String newString = randomString();
+        assertAdd(true, (newString));
+        assertContains(true, (newString));
+        assertSize(1);
+        assertAdd(false, (newString));
+        assertSize(1);
+        String suffix = newString.substring(1);
+        String prefix = newString.substring(0, newString.length() - 1);
+        assertContains(false, (suffix));
+        assertContains(false, (prefix));
+        assertSize(1);
+        assertRemove(false, (suffix));
+        assertRemove(false, (prefix));
+        assertRemove(true, (newString));
+        assertSize(0);
+        assertContains(false, (suffix));
+        assertContains(false, (prefix));
+        assertContains(false, (newString));
+        assertRemove(false, (newString));
+        assertContains(false, (suffix));
+        assertContains(false, (prefix));
+        assertContains(false, (newString));
+    }
+
+    @Test
+    public void typicalCase2() {
+        StringSetImpl stringSet = new StringSetImpl();
+        String newString = randomString();
+        assertAdd(true, (newString));
+        assertAdd(false, (newString));
+
+        String suffix = newString.substring(1);
+        String prefix = newString.substring(0, newString.length() - 1);
+
+        final int prefixTestCount = 100;
+        for (int i = 0; i < prefixTestCount; ++i) {
+            String checkPrefix = prefix.substring(0, RANDOMIZER.nextInt(prefix.length()));
+            assertHowManyStartsWithPrefix((checkPrefix), 1);
+        }
+
+        assertSize(1);
+        assertRemove(false, (suffix));
+        assertRemove(false, (prefix));
+        assertRemove(true, (newString));
+        assertSize(0);
+    }
+
     private static String randomString() {
         int size = 1 + RANDOMIZER.nextInt(MAX_STRING_SIZE);
         StringBuilder sb = new StringBuilder();
@@ -41,107 +157,54 @@ public class StringSetImplTest {
         return sb.toString();
     }
 
-    @Test
-    public void testSimple() {
-        StringSetImpl stringSet = new StringSetImpl();
-
-        assertTrue(stringSet.add("abc"));
-        assertTrue(stringSet.contains("abc"));
-        assertEquals(1, stringSet.size());
-        assertEquals(1, stringSet.howManyStartsWithPrefix("abc"));
+    private StringSetImpl testSerialization(StringSetImpl olds) {
+        StringSetImpl s = new StringSetImpl();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        olds.serialize(out);
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        s.deserialize(in);
+        return s;
     }
 
-    @Test
-    public void myTest() {
-        StringSetImpl stringSet = new StringSetImpl();
+    private StringSetImpl lastStringSet() {
+        int size = stringSets.size();
+        assert(size > 0);
+        return stringSets.get(size - 1);
+    }
 
-        ArrayList<String> alreadyAdded = new ArrayList<>();
-        for (int i = 0; i < TESTS_COUNT; i++) {
-            String newString = randomString();
-            assertEquals(stringSet.size(), alreadyAdded.size());
-            if (alreadyAdded.contains(newString)) {
-                assertFalse(stringSet.add(newString));
-                assertEquals(stringSet.size(), alreadyAdded.size());
-            } else {
-                assertTrue(stringSet.add(newString));
-                alreadyAdded.add(newString);
-            }
+    private void assertAdd(boolean answer, String s) {
+        if (testSerializationBetweenStates) {
+            stringSets.add(testSerialization(lastStringSet()));
         }
-
-        for (String s : alreadyAdded) {
-            assertFalse(stringSet.add(s));
-            assertTrue(stringSet.contains(s));
-        }
-
-        for (int i = 0; i < TESTS_COUNT; i++) {
-            String s = randomString();
-            assertEquals(alreadyAdded.contains(s), stringSet.contains(s));
+        for (StringSetImpl stringSet : stringSets) {
+            assertEquals(answer, stringSet.add(s));
         }
     }
 
-    @Test
-    public void stringSetSpecificCases() {
-        StringSetImpl stringSet = new StringSetImpl();
-        String newString = randomString();
-        assertEquals(stringSet.size(), 0);
-        assertTrue(stringSet.add(newString));
-        assertEquals(stringSet.size(), 1);
-        assertFalse(stringSet.add(newString));
-        assertEquals(stringSet.size(), 1);
-        String prefix = newString.substring(0, newString.length() - 1);
-        assertTrue(stringSet.add(prefix));
-        assertTrue(stringSet.remove(newString));
-        assertTrue(stringSet.contains(prefix) && !stringSet.contains(newString));
-    }
-
-    @Test
-    public void typicalCase1() {
-        StringSetImpl stringSet = new StringSetImpl();
-        String newString = randomString();
-        assertTrue(stringSet.add(newString));
-        assertTrue(stringSet.contains(newString));
-        assertEquals(stringSet.size(), 1);
-        assertFalse(stringSet.add(newString));
-        assertEquals(stringSet.size(), 1);
-        String suffix = newString.substring(1);
-        String prefix = newString.substring(0, newString.length() - 1);
-        assertFalse(stringSet.contains(suffix));
-        assertFalse(stringSet.contains(prefix));
-        assertEquals(stringSet.size(), 1);
-        assertFalse(stringSet.remove(suffix));
-        assertFalse(stringSet.remove(prefix));
-        assertTrue(stringSet.remove(newString));
-        assertEquals(stringSet.size(), 0);
-        assertFalse(stringSet.contains(suffix));
-        assertFalse(stringSet.contains(prefix));
-        assertFalse(stringSet.contains(newString));
-        assertFalse(stringSet.remove(newString));
-        assertFalse(stringSet.contains(suffix));
-        assertFalse(stringSet.contains(prefix));
-        assertFalse(stringSet.contains(newString));
-    }
-
-    @Test
-    public void typicalCase2() {
-        StringSetImpl stringSet = new StringSetImpl();
-        String newString = randomString();
-        assertTrue(stringSet.add(newString));
-        assertFalse(stringSet.add(newString));
-
-        String suffix = newString.substring(1);
-        String prefix = newString.substring(0, newString.length() - 1);
-
-        final int prefixTestCountMax = 10;
-        final int prefixTestCount = RANDOMIZER.nextInt(prefixTestCountMax);
-        for (int i = 0; i < prefixTestCount; ++i) {
-            String checkPrefix = prefix.substring(0, RANDOMIZER.nextInt(prefix.length()));
-            assertEquals(stringSet.howManyStartsWithPrefix(checkPrefix), 1);
+    private void assertContains(boolean answer, String s) {
+        for (StringSetImpl stringSet : stringSets) {
+            assertEquals(answer, stringSet.contains(s));
         }
+    }
 
-        assertEquals(stringSet.size(), 1);
-        assertFalse(stringSet.remove(suffix));
-        assertFalse(stringSet.remove(prefix));
-        assertTrue(stringSet.remove(newString));
-        assertEquals(stringSet.size(), 0);
+    private void assertRemove(boolean answer, String s) {
+        if (testSerializationBetweenStates) {
+            stringSets.add(testSerialization(lastStringSet()));
+        }
+        for (StringSetImpl stringSet : stringSets) {
+            assertEquals(answer, stringSet.remove(s));
+        }
+    }
+
+    private void assertSize(int answer) {
+        for (StringSetImpl stringSet : stringSets) {
+            assertEquals(answer, stringSet.size());
+        }
+    }
+
+    private void assertHowManyStartsWithPrefix(String prefix, int answer) {
+        for (StringSetImpl stringSet : stringSets) {
+            assertEquals(answer, stringSet.howManyStartsWithPrefix(prefix));
+        }
     }
 }
